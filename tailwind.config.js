2848401b9b80acc5e6a4a4b5f2de50fe502cb726
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-10-11 09:10:55 +0300

    add colors into tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#F9FBFF',
+        primary: '#701ADF',
+        secondary: '#7BDFEE',
+        'primary-text': '#343840',
+        'secondary-text': '#8C909D',
+        'disabled-text': '#8A8A8A',
+        'disabled-button': '#D3D3D3',
+        'border-primary': '#CCDAFB',
+        'border-secondary': '#E6EAF6',
+        'mercan-green': '#2ED6AD',
+        'mercan-red': '#EB5757',
+      },
+    },
   },
   plugins: [],
-}
+};
